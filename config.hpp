67ce434928fc73132d2b552d310dd02c222cdf9f
nicolassn013 <kati.nsoares@gmail.com>
2026-06-11 18:49:19 +0000

    config feita

---
+++ config.hpp
@@ -0,0 +1,15 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+
+namespace Config {
+
+    constexpr int NIVEL_BAIXO = 27;
+    constexpr int NIVEL_ALTO = 80;
+
+    constexpr double PRESSAO_ALTA = 7.0;
+
+    constexpr int CICLOS_SENSOR_TRAVADO = 10;
+
+}
+
+#endif
